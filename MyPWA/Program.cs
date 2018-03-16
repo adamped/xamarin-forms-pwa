3736ac759dec3531800bc68c66b952a2061cd307
Adam Pedley <adam.pedley@gmail.com>
2018-03-16 14:37:54 +1100

    Add project files.

---
+++ MyPWA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyPWA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
